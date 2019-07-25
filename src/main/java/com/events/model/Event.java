78e815b99f06849d3fe2c9aec733202aaf51ce6b
Andris Andermann <andris.andermann@terra.com.br>
2019-07-25 13:06:47 -0300

    Create Event - First Steps

---
+++ src/main/java/com/events/model/Event.java
@@ -0,0 +1,40 @@
+package com.events.model;
+
+/**
+ * @author aandermann
+ */
+public class Event {
+
+	private String name;
+	private String location;
+	private String date;
+	private String time;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
+	
+
+}
